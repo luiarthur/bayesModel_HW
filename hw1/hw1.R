d9eb14a1aed00039c8949c81ea4a0a8e18355044
luiarthur <luiarthur@gmail.com>
2016-04-06 15:37:31 -0700

    yes

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -1,5 +1,6 @@
 #install.packages("LearnBayes")
 library(LearnBayes)
+source("plotmap.R")
 
 dat <- cancermortality
 
@@ -8,3 +9,19 @@ n <- dat$n
 J <- nrow(dat)
 
 
+loglike_plus_logprior <- function(uv) {
+  lbeta <- function(a,b) lgamma(a) + lgamma(b) - lgamma(a+b)
+  u <- uv[1]
+  v <- uv[2]
+
+  u <- ifelse(u < .0001,.0001,u)
+  mu <- 1/ (exp(-u)+1)
+  tau <- exp(v)
+  
+  v - 2*log(1+exp(v)) + sum(lbeta(mu*tau+y, tau-mu*tau+n+y) - lbeta(mu*tau,tau-mu*tau))
+}
+
+optim(c(-10,10),fn=function(x) -loglike_plus_logprior(x),hessian=TRUE)
+s <- expand.grid(seq(-10,10,len=100), seq(0,100,len=100))
+ys <- apply(s,1,function(x) -loglike_plus_logprior(x))
+plotmap(ys,s)
