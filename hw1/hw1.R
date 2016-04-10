f6aee9a52a10054fec6d648e0ffd37a8ca9b3cfe
Arthur Lui <luiarthur@gmail.com>
2016-04-10 01:09:37 -0700

    yes

--- hw1/hw1.R
+++ hw1/hw1.R
@@ -18,10 +18,32 @@ loglike_plus_logprior <- function(uv) {
   mu <- 1/ (exp(-u)+1)
   tau <- exp(v)
   
-  v - 2*log(1+exp(v)) + sum(lbeta(mu*tau+y, tau-mu*tau+n+y) - lbeta(mu*tau,tau-mu*tau))
+  v - 2*log(1+exp(v)) + sum(lbeta(mu*tau+y, n+tau-mu*tau-y) - lbeta(mu*tau,tau-mu*tau))
 }
 
-optim(c(-10,10),fn=function(x) -loglike_plus_logprior(x),hessian=TRUE)
+
+optim(c(-7,7),fn=function(x) -loglike_plus_logprior(x),hessian=TRUE)
 s <- expand.grid(seq(-10,10,len=100), seq(0,100,len=100))
 ys <- apply(s,1,function(x) -loglike_plus_logprior(x))
 plotmap(ys,s)
+
+
+h <- function(uv) {
+  loglike_plus_logprior(uv) / -J
+}
+
+h.star <- function(uv) {
+  v <- uv[2]
+  h(uv) - (- v - 2*log(1+exp(v))) / J
+}
+
+
+s1 <- expand.grid(seq(-10,10,len=100), seq(0,100,len=100))
+ys1 <- apply(s1,1,function(x) h(x))
+plotmap(ys1,s1)
+optim(c(7,20),fn=function(x) h(x),hessian=TRUE)
+
+s2 <- expand.grid(seq(-10,10,len=100), seq(0,100,len=100))
+ys2 <- apply(s2,1,function(x) h.star(x))
+plotmap(ys1,s1)
+optim(c(7,7),fn=function(x) h.star(x),hessian=TRUE)
