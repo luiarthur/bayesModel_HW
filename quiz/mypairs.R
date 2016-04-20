f0c114423d52b93947ecdc13397026e0ca249729
Arthur Lui <luiarthur@gmail.com>
2016-04-20 11:40:38 -0700

    yes

--- quiz/mypairs.R
+++ quiz/mypairs.R
@@ -2,26 +2,27 @@ my.pairs <- function(M,digits=3) {
   cnames <- colnames(M)
   k <- ncol(M)
   corrs <- cor(M)
-  par(mfrow=c(k,k))
+  par(mfrow=c(k,k),mar=c(0,0,0,0)+2)
   for (i in 1:k) {
     if (i>1) {
       for (j in 1:(i-1)) { 
-        plot(1, type="n", axes=F, xlab="", ylab="",
-             main=paste0("Corr (",cnames[i],",",cnames[j],")")) # empty plot
+        plot(1, type="n", axes=F, xlab="", ylab="",main="")
+             #main=paste0("Corr (",cnames[i],", ",cnames[j],")")) # empty plot
         r <- round(corrs[i,j],digits)
         cex.cor <- max(.8/strwidth(format(r)) * abs(r),1)
         text(1,labels=r,cex=cex.cor,col="grey")
       }  
     }
     
-    hist(M[,i],prob=TRUE,main=paste("Histogram of",cnames[i]),bty="n",fg="grey",
-         col="grey",border="white",xlab="")
+    hist(M[,i],prob=TRUE,bty="n",fg="grey",col="grey",border="white",xlab="",
+         main=cnames[i])
+         #main=paste("Histogram of",cnames[i]))
 
     if (i<k) {
       for (j in (i+1):k) {
         plot(M[,c(j,i)],xlab=cnames[j],ylab=cnames[i],pch=20,
-             main=paste(cnames[i],"-",cnames[j]),
-             bty="n",fg="grey")
+             bty="n",fg="grey",main="")
+             #main=paste(cnames[i],"-",cnames[j])_
       }
     }  
   }
