1fb586b891d4b50479232a84e6b3eff8de55c859
Arthur Lui <luiarthur@gmail.com>
2016-04-27 16:06:49 -0700

    yes

--- hw2/jl/testplot.jl
+++ hw2/jl/testplot.jl
@@ -1,38 +1,11 @@
 using RCall, Distributions, PyPlot
+rgb = R"rgb"
 
-R"
-source('../../R_Functions/plotPost.R',chdir=TRUE)
+rgb(.5,.5,.5,.5)
 
-Y <- as.matrix(iris[,1:2])
-Y_bar <- apply(Y,2,mean)
-c_rows <- t(apply(Y,1,function(y) y - Y_bar))
-C_mat <- matrix(0,ncol(Y),ncol(Y))
-for (i in 1:nrow(c_rows)) {
-  C_mat <- C_mat + c_rows[i,] %*% t(c_rows[i,])
-}
-
-# Prior Params:
-s <- 1
-r <- 10
-k <- length(Y_bar)
-n <- nrow(Y)
-S <- diag(k)
-m <- Y_bar
-
-# Posterior Params:
-m_post <- (s*m + n*Y_bar) / (s+n) 
-s_post <- s+n
-r_post <- r+n
-iS_post <- solve(S) + C_mat + s*n/(s+n) * (Y_bar-m) %*% t(Y_bar-m)
-S_post <- solve(iS_post) # IMPORTANT!!!
-
-B <- 100000
-"
-@rget Y Y_bar C_mat m_post s_post r_post iS_post S_post B
-plot_posts = R"plot.posts"
-plot_posts(Y) 
-
-for color in ["red", "green", "blue"]
+subplot()
+subplot(221)
+for color in ["red", "lime", "cornflowerblue"]
     n = 750
     sims = randn(n,2)
     x = sims[:,1]; y = sims[:,2]
@@ -45,3 +18,20 @@ legend()
 PyPlot.xlabel("X")
 PyPlot.ylabel("my Y")
 grid(true)
+
+
+subplot(222)
+x = randn(100)
+PyPlot.plt[:hist](x,color="cornflowerblue",edgecolor="white")
+
+
+subplot(223)
+plot(x)
+
+subplot(224)
+PyPlot.plt[:hist](x,color="cornflowerblue",edgecolor="white")
+# Have to use kde
+
+#=
+include("testplot.jl")
+=#
