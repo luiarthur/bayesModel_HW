22129ae8fd45586a3f343543614d6a972169fb7a
Arthur Lui <luiarthur@gmail.com>
2016-04-27 10:22:53 -0700

    yes

--- hw2/jl/hw2.jl
+++ hw2/jl/hw2.jl
@@ -1,5 +1,42 @@
-using RCall, Distributions
+using RCall, Distributions, PyPlot
 
-R"source('../../R_Functions/plotPost.R',chdir=TRUE)"
+R"
+source('../../R_Functions/plotPost.R',chdir=TRUE)
 
-R"plot.posts()"
+Y <- as.matrix(iris[,1:2])
+Y_bar <- apply(Y,2,mean)
+c_rows <- t(apply(Y,1,function(y) y - Y_bar))
+C_mat <- matrix(0,ncol(Y),ncol(Y))
+for (i in 1:nrow(c_rows)) {
+  C_mat <- C_mat + c_rows[i,] %*% t(c_rows[i,])
+}
+
+# Prior Params:
+s <- 1
+r <- 10
+k <- length(Y_bar)
+n <- nrow(Y)
+S <- diag(k)
+m <- Y_bar
+
+# Posterior Params:
+m_post <- (s*m + n*Y_bar) / (s+n) 
+s_post <- s+n
+r_post <- r+n
+iS_post <- solve(S) + C_mat + s*n/(s+n) * (Y_bar-m) %*% t(Y_bar-m)
+S_post <- solve(iS_post) # IMPORTANT!!!
+
+B <- 100000
+"
+@rget Y Y_bar C_mat m_post s_post r_post iS_post S_post B
+plot_posts = R"plot.posts"
+plot_posts(Y) 
+
+fig = figure("tt",figsize=(10,10))
+p = plot(Y[:,1],Y[:,2])
+xlabel="Sepal Length"
+ylabel="Sepal Width"
+title="hi"  
+grid("on")
+legend(loc="upper right")
+show.p
