027e5c3fdcfe6e835125a0973cc6ac3e5f10c66f
Arthur Lui <luiarthur@gmail.com>
2016-06-03 14:21:34 -0700

    yes

--- hw/fye/code/ozone.R
+++ hw/fye/code/ozone.R
@@ -1,12 +1,18 @@
 source("../../quiz/mypairs.R",chdir=TRUE)
+system("mkdir -p ../report/figs")
+
+
+# Data:
 dat <- read.table("../dat/ozone.dat",header=TRUE)
 nrow(dat)
 colnames(dat)
-
 # ozone concentration: ppb
 #     solar radiation: 0-low; 1-moderate to high
 #      daily max temp: F
 #          wind speed: mph
 
+
 # EXPLORATORY ANALYSIS & FINDINGS
-my.pairs(dat)
+pdf("../report/figs/pairs.pdf")
+  my.pairs(dat)
+dev.off()
