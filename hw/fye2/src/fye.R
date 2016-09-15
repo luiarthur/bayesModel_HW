977a1ddf2096ea872ce33252f77b76c7b0116311
Arthur Lui <luiarthur@gmail.com>
2016-09-15 15:02:00 -0700

    yes

--- hw/fye2/src/fye.R
+++ hw/fye2/src/fye.R
@@ -1,6 +1,8 @@
+set.seed(1)
 source("gibbs.R")
 source("../../quiz/mypairs.R",chdir=TRUE)
 dat <- read.csv("../dat/fye.dat",header=TRUE)
+rig <- function(n,shape,rate) 1/rgamma(n,shape=shape,rate=rate)
 
 # (a)
 cMR <- dat[,"ctrlMortRate"]/100
@@ -33,6 +35,7 @@ pdf("../tex/img/tcy.pdf")
 plotTCY()
 dev.off()
 
+### M1
 V <- cMR*(1-cMR)/cN + tMR*(1-tMR)/tN
 stopifnot(length(V) == length(y) && length(y) == 6)
 
@@ -46,28 +49,42 @@ q.post <- function(p) qnorm(p,mu.post.mean.M1,sd=sqrt(mu.post.var.M1))
 F.post <- function(x) pnorm(x,mu.post.mean.M1,sd=sqrt(mu.post.var.M1))
 
 M1.plot.post <- function() {
-  curve(f.post,from=q.post(1e-5), to=q.post(1-1e-5), cex.lab=1.5,
-        col='white',bty="n",fg="grey", ylab="density",xlab=expression(mu))
+  curve(f.post,from=q.post(1e-5), to=q.post(1-1e-5), cex.lab=1.3,
+        col='white',bty="n",fg="grey", ylab="Density",xlab=expression(mu))
   color.fn(f.post,from=q.post(1e-5), to=q.post(1-1e-5),
            col=col.mult('dodgerblue','darkgrey'))
-  color.fn(f.post,from=q.post(.025), to=q.post(.975),col.mult('navy'))
-  abline(v=c(mu.post.mean.M1,0),col=c('red','grey'),lwd=2)
-  legend("topright",bty="n",cex=1,text.col="grey30",
-        legend=paste(c("Mean =", "SD =","P(mu > 0 | y, M1) ="),
+  color.fn(f.post,from=0, to=1,col.mult('navy'))
+  abline(v=c(mu.post.mean.M1),col=c('red'),lwd=2)
+  legend("topleft",bty="n",cex=1,text.col="grey30",
+        legend=paste(c("Mean =", "SD =",
+                       "HPD Lower =", "HPD Upper =",
+                       "P(mu > 0 | y, M1) ="),
                       round(c(mu.post.mean.M1,sqrt(mu.post.var.M1),
-                              1-F.post(0)),4)))
+                              q.post(c(.025,.975)), 1-F.post(0)),4)))
 }
 
 pdf("../tex/img/m1Post.pdf")
 M1.plot.post()
 dev.off()
 
+post.pred.M1 <- t(sapply(1:10000,function(x) rnorm(6,r.post(1),V)))
+apply(post.pred.M1,2,mean)
+
 q.post(c(.025,.975))
 
-#rig(n,a,b) <- 1/rgamma(n,a,b)
-# IG(3,1) => mean=.5, sd=.5
 
-out <- gibbs(y=y,V=V,B=5000,burn=5000)
+### M2
+# rig(n,a,b) <- 1/rgamma(n,a,b)
+# IG(3,1) => mean=.5, sd=.5
+source("gibbs.R")
+# This is when M2 -> M1
+#out <- gibbs(y=y,V=V,B=10000,burn=50000,params=list(siga=100,sigb=.001))
+# This understates the certainty of mu
+#out <- gibbs(y=y,V=V,B=10000,burn=5000,params=list(siga=3,sigb=1))
+# This also, this has variance of infty
+#out <- gibbs(y=y,V=V,B=10000,burn=5000,params=list(siga=1,sigb=1))
+# This maybe: because variance can't go above .16
+out <- gibbs(y=y,V=V,B=10000,burn=5000,params=list(siga=3,sigb=.3))
 
 pdf("../tex/img/m2MuS2Post.pdf")
 plot.posts(out[,1:2],names=colnames(out)[1:2],cex.a=1,rng.x=c(0,.999))
@@ -78,4 +95,61 @@ simple.plot.posts(out[,-c(1:2)],cex.a=.7)
 dev.off()
 
 mu.post <- out[,"mu"]
-(M2.mu.p <- mean(mu.post > 0)) # 5.27
+
+pdf("../tex/img/m2MuPost.pdf")
+  simple.plot.post(mu.post,trace=TRUE,main="",xlab=expression(mu),cex.lab=1.3)
+  color.den(density(mu.post),from=-1,to=1,col.a=col.mult("navy"),
+            col.d='white',add=TRUE)
+  color.den(density(mu.post),from=-1,to=0,col.a="dodgerblue",
+            col.d='transparent',add=TRUE)
+  lines(density(mu.post),col="white",lwd=3)
+  legend("topleft",bty="n",cex=1,text.col="grey30",
+        legend=paste(c("Mean =", "SD =",
+                       "HPD Lower =", "HPD Upper =",
+                       "P(mu > 0 | y, M1) ="),
+                      round(c(mean(mu.post),sd(mu.post),
+                              quantile(mu.post,c(.025,.975)),
+                              mean(mu.post>0) ),4)))
+  abline(v=mean(mu.post),col='red',lwd=2)
+dev.off()
+#(M2.mu.p <- mean(mu.post > 0)) # .572
+# mean(rep(cMR - tMR,(cN+tN)) > 0) # .5817 # THIS IS WHAT I SHOULD EXPECT!!!
+
+apply(out[,-c(1:2)],2,mean)
+
+#########
+#x <- rig(1000000,100,.001); mean(x); sd(x)
+#x <- rig(1000000,3,1); mean(x); sd(x)
+#x <- rig(1000000,3,.3); mean(x); sd(x)
+
+#sd(sapply(1:10000,function(x) sd(runif(5,-1,1))))
+
+### DIC
+DIC.M1 <- function(post.samps) {
+  d <- function(mu) {
+    stopifnot(length(y) == length(V))
+    -2 * sum(dnorm(y,mu,sqrt(V),log=TRUE))
+  }
+  ds <- sapply(post.samps, d)
+  print(mean(ds))
+  print(var(ds))
+  mean(ds) + var(ds)/2
+}
+
+DIC.M2 <- function(post.th) {
+  stopifnot(ncol(post.th) == length(y))
+  d <- function(th) {
+    stopifnot(length(y) == length(V))
+    stopifnot(length(y) == length(th))
+    -2 * sum(dnorm(y,th,sqrt(V),log=TRUE))
+  }
+  ds <- apply(post.th,1, d)
+  print(mean(ds))
+  print(var(ds))
+  mean(ds) + var(ds)/2
+}
+
+dic.m1 <- DIC.M1(r.post(10000))
+dic.m2 <- DIC.M2(out[,-c(1:2)])
+
+dic.m2 - dic.m1
